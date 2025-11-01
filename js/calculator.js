50cd6c64d50359ba177b507d8d2335528e536616
AlexSokol <astrasokol@yandex.ru>
2025-11-01 20:36:04 +0300

    Initial commit

---
+++ js/calculator.js
@@ -0,0 +1,10 @@
+// function dropdown() {
+//   return {
+//     open: false,
+//     toggle() {
+//       this.open = !this.open;
+//     },
+//   };
+// }
+
+//document.addEventListener('alpine:init', () => {});
